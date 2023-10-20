45fd5d9380304ee5a95640d0c32d7cb0667d5ac9
User7558 <82788197+User7558@users.noreply.github.com>
2023-10-20 18:33:46 -0300

    Create ex008.js

---
+++ aula008/ex008.js
@@ -0,0 +1,9 @@
+var vel = 67
+console.log(`Seu carro esta a ${vel} km/h.`)
+
+if(vel>60){
+  console.log('Você ultrapassou o limite de velocidade, MULTADO!')
+}
+else{
+  console.log('Dirija com segurança: use o cinto.')
+}
